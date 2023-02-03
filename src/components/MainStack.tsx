129ea85e33a1f18ba92b03661f76467bc985a9c9
ittp <61617660+ittp@users.noreply.github.com>
2023-02-03 05:12:08 +0300

    1 file modified

--- src/components/MainStack.tsx
+++ src/components/MainStack.tsx
@@ -1,9 +1,9 @@
 import { BaseNavigationContainer } from '@react-navigation/core';
-import * as React from "react";
-import { stackNavigatorFactory } from "react-nativescript-navigation";
+import * as React from 'react';
+import { stackNavigatorFactory } from 'react-nativescript-navigation';
 
-import { ScreenOne } from "./ScreenOne";
-import { ScreenTwo } from "./ScreenTwo";
+import { ScreenOne } from './ScreenOne';
+import { ScreenTwo } from './ScreenTwo';
 
 const StackNavigator = stackNavigatorFactory();
 
@@ -11,24 +11,18 @@ const StackNavigator = stackNavigatorFactory();
  * The main stack navigator for the whole app.
  */
 export const MainStack = () => (
-    <BaseNavigationContainer>
-        <StackNavigator.Navigator
-            initialRouteName="Screen One"
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: "white",
-                },
-                headerShown: true,
-            }}
-        >
-            <StackNavigator.Screen
-                name="One"
-                component={ScreenOne}
-            />
-            <StackNavigator.Screen
-                name="Two"
-                component={ScreenTwo}
-            />
-        </StackNavigator.Navigator>
-    </BaseNavigationContainer>
+  <BaseNavigationContainer>
+    <StackNavigator.Navigator
+      initialRouteName="Screen One"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'white',
+        },
+        headerShown: true,
+      }}
+    >
+      <StackNavigator.Screen name="One" component={ScreenOne} />
+      <StackNavigator.Screen name="Two" component={ScreenTwo} />
+    </StackNavigator.Navigator>
+  </BaseNavigationContainer>
 );
